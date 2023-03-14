43e8cc640caf3a9dbac193603248b31939ac468c
abc90155 <38838173+abc90155@users.noreply.github.com>
2023-03-14 22:37:23 +0800

    block secret key

--- centerWeb/settings.py
+++ centerWeb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@2e!^catl3ncg!zq*a)ue^0911!+@x)b%+z0^ckbos4+2wl&hc'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
